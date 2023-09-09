81daab057d936937cc8ba5e65eec3397a8f974a2
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2023-09-09 21:49:31 +0300

    Добавлен базовый функционал сервера

---
+++ ConBrain/Model/UserDbContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+namespace ConBrain.Model
+{
+    /// <summary>
+    /// База данных с параметрами пользователей
+    /// </summary>
+    public class UserDbContext:DbContext
+    {
+        public UserDbContext(string connect):base()
+        {
+            _connect = connect;
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Person> People { get; set; }
+        private readonly string _connect;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(_connect);
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Person>().Property(i => i.Nick).IsRequired().IsUnicode();
+            builder.Entity<Person>().Property(i=>i.Phone).IsRequired().IsUnicode();
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Nick { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Family { get; set; } = "";
+        public string? LastName { get; set; }
+        public string Phone { get; set; } = "";
+        public IEnumerable<Person>? Friends { get; set; }
+        public IEnumerable<Message>? Messages { get; set; }
+    }
+
+    public class Message
+    {
+        public Person Target { get; set; }
+        public Person Sender { get; set; }
+        public string Content { get; set; }
+    }
+
+}
--- ConBrain/Program.cs
+++ ConBrain/Program.cs
@@ -1,3 +1,7 @@
+using ConBrain.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ConBrain
 {
     public class Program
@@ -5,11 +9,42 @@ namespace ConBrain
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            builder.Services.AddSingleton(new UserDbContext(builder.Configuration.GetSection("ConnectionStrings.sqliteUsers").Value!));
+            
+            var app = builder.Build();            
 
-            app.MapGet("/", () => "Hello World!");
+            app.Map("/user", OnUserMap);
 
             app.Run();
+        }
+
+        private static void OnUserMap(IApplicationBuilder builder)
+        {
+            builder.Map("/register", (IApplicationBuilder b) => SendHTMLFileMap(b, "register.html"));
+
+        }
+        private static void OnRegisterUserMap(IApplicationBuilder builder)
+        {
+            builder.Map("/reply", OnReplyRegisterUserMap);
+
+        }
+        private static void OnReplyRegisterUserMap(IApplicationBuilder builder)
+        {
+            //var context = builder.ApplicationServices.GetService<HttpContext>();
+            //var person = (Person)context.Request.Form.First().Value;//Доделать
+            //var db = builder.ApplicationServices.GetService<UserDbContext>();
+            //db.People.Add(person);
+            //db.Update(person);
+            //db.SaveChanges();
+        }
+        private static void SendHTMLFileMap(IApplicationBuilder builder, string path)
+        {
+            builder.Run(async (context) =>
+            {
+                var respore = context.Response;
+                respore.ContentType = "text/html; charset=utf-8";
+                await respore.SendFileAsync(path);
+            });
         }
     }
 }
