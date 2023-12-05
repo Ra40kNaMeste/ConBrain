161ae6db756fdac1a7b066d70b51d5e839b9cd6a
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2023-12-05 22:50:13 +0300

    Добавлена форма регистрации и исправлены некоторые ошибки отображения

---
+++ ConBrain/Controllers/AuthorizationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConBrain.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
