7d850a99f913dfaeca3f116ad18ba738a6182f17
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2023-12-13 19:15:52 +0300

    Добавлена валидация пароля в форме регистрации. Изменён дизайн

---
+++ ConBrain/wwwroot/js/PasswordValidateScript.js
@@ -0,0 +1,14 @@
+﻿function validate(e) {
+    const secondPassword = document.getElementsByName("repeatpass")[0];
+
+    if (secondPassword.value != document.getElementsByName("pass")[0].value) {
+        secondPassword.value = "";
+        secondPassword.setAttribute("class", "incorrectInput");
+        e.preventDefault();
+    }
+    
+}
+
+const sendBtn = document.getElementsByName("send")[0];
+
+sendBtn.addEventListener("click", validate);
