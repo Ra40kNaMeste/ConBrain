eb217be5a14ea78f1343dd4854f7794e672b228a
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2024-02-01 18:15:13 +0300

    Добавлена страница друзей

---
+++ ConBrain/wwwroot/js/personPage/setLinkInPeopleBody.js
@@ -0,0 +1,13 @@
+﻿const peopleBodies = document.getElementsByName("peopleBody");
+
+peopleBodies.forEach(body => {
+    for (tbody of body.children) {
+        for (row of tbody.children) {
+            row.addEventListener("click", (e) => onPersonClick(row.children[1].innerText));
+        }
+        
+    }
+})
+function onPersonClick(nick) {
+    window.location.href = `../id=${nick}`;
+}
