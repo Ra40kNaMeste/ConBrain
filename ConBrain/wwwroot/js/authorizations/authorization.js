121bf01121b5ffcfaf88c13a8b2b51c56384fa64
Ra40kNaMeste <aleksiei.smirnov.2001@gmail.com>
2024-02-11 22:11:38 +0300

    Добавлен вывод ошибки ввода логина.

--- ConBrain/wwwroot/js/authorizations/authorization.js
+++ ConBrain/wwwroot/js/authorizations/authorization.js
@@ -1,7 +1,5 @@
-﻿const form = document.getElementsByName("loginform")[0];
-const values = [].slice.call(document.getElementsByClassName("sendInput"));
-async function saveToken(e) {
-    e.preventDefault();
+﻿export async function fetchWithAddressString(e) {
+    const values = [].slice.call(document.getElementsByClassName("sendInput"));
     const target = e.currentTarget;
 
     //Собираем тело для отправки
@@ -12,30 +10,22 @@ async function saveToken(e) {
         formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-    console.log(formBody);
     //отправляем
     const response = await fetch(target.action, {
         method: target.method,
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: formBody
     });
-    const data = await response.json();
-    console.log(data);
+    return response;
+}
+export async function saveToken(response) {
     //получаем ответ
     if (response.ok === true) {
+        const data = await response.json();
         document.cookie = "token = " + data.token;
         window.location.replace("../home")
+        return true;
     }
-    else {
-        if (data != null) {
-            for (result of data) {
-                for (member of result.memberNames) {
-                    const box = values.find(i => i.name.toLowerCase() == str);
-                    box.setCustomValidity(result.errorMessage);
-                    box.reportValidity();
-                }
-            }
-        }
-    }
+    return false;
 }
-form.addEventListener("submit", saveToken);
+
---
+++ ConBrain/wwwroot/js/authorizations/login.js
@@ -0,0 +1,16 @@
+﻿import { fetchWithAddressString, saveToken } from "./authorization.js";
+const form = document.getElementsByName("loginform")[0];
+const values = [].slice.call(document.getElementsByClassName("sendInput"));
+
+form.addEventListener("submit", async e => {
+    e.preventDefault();
+    const response = await fetchWithAddressString(e);
+    if (await saveToken(response) == false) {
+        console.log(values);
+        values.forEach(value => {
+            value.value = "";
+            value.setCustomValidity("incorrect login or password");
+            value.reportValidity();
+        })
+    }
+});
